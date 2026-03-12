b241ea0f514020137a09fd609178b7c6344b798f
u-tv <vikkythakur1995@gmail.com>
2026-03-12 21:39:49 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,10 @@
-const cacheName = 'utv-v2';
-const staticAssets = ['./', './index.html', './manifest.json'];
+const cacheName = 'utv-v5-earning';
+const assets = ['./', './index.html', './manifest.json'];
 
-self.addEventListener('install', async e => {
-  const cache = await caches.open(cacheName);
-  await cache.addAll(staticAssets);
-  return self.skipWaiting();
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-self.addEventListener('activate', e => {
-  self.clients.claim();
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
-
-self.addEventListener('fetch', async e => {
-  const req = e.request;
-  const url = new URL(req.url);
-  if (url.origin === location.origin) {
-    e.respondWith(cacheFirst(req));
-  } else {
-    e.respondWith(networkAndCache(req));
-  }
-});
-
-async function cacheFirst(req) {
-  const cache = await caches.open(cacheName);
-  const cached = await cache.match(req);
-  return cached || fetch(req);
-}
-
-async function networkAndCache(req) {
-  const cache = await caches.open(cacheName);
-  try {
-    const refresh = await fetch(req);
-    await cache.put(req, refresh.clone());
-    return refresh;
-  } catch (e) {
-    const cached = await cache.match(req);
-    return cached;
-  }
-}
