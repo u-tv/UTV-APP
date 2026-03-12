0984e16a70c30a6e9d1f16bd69e1f265d883fd82
u-tv <vikkythakur1995@gmail.com>
2026-03-12 19:56:54 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,11 @@
+const CACHE = 'utv-pwa-v1';
+self.addEventListener('install', e => {
+    e.waitUntil(caches.open(CACHE).then(cache => 
+        cache.addAll(['/','/index.html','/manifest.json'])
+    ));
+});
+self.addEventListener('fetch', e => {
+    e.respondWith(caches.match(e.request).then(resp => 
+        resp || fetch(e.request)
+    ));
+});
