f89f17207d782f3b67e50119b608d5148a96911c
u-tv <vikkythakur1995@gmail.com>
2026-03-12 20:10:47 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,3 @@
-const CACHE = 'utv-pwa-v1';
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(cache => 
-        cache.addAll(['/','/index.html','/manifest.json'])
-    ));
-});
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(resp => 
-        resp || fetch(e.request)
-    ));
-});
+const CACHE='utv-v1';
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/', '/index.html', '/manifest.json']))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
