81112d3d91617a99f0805cfbbceace93b468c003
u-tv <vikkythakur1995@gmail.com>
2026-03-12 22:38:23 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,8 @@
-const cacheName = 'utv-movie-hub-v2';
-const assets = ['./', './index.html', './manifest.json'];
-
+const cacheName = 'utv-v11';
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+  self.skipWaiting();
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(['./', './index.html', './manifest.json'])));
 });
-
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
