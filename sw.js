f23e1f57a8a405229c0bbd5cbcae1e5cb545104b
u-tv <vikkythakur1995@gmail.com>
2026-03-12 20:23:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,24 @@
-const CACHE='utv-v1';
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/', '/index.html', '/manifest.json']))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE_NAME = 'u-tv-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Service Worker Install करना
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// नेटवर्क से फाइलें फेच करना
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
